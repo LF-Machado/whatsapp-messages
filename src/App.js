8cf207dc53c03ce9e8aa3dd9c2845fba6198b70c
Luis Felipe Machado <luis.felipe.machado13@gmail.com>
2021-06-24 16:37:26 -0500

    Added styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Message from "./components/Message";
+import { Navbar } from "react-bootstrap";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <Message />;
+  return (
+    <div className="App">
+      <Navbar className="navbar align-items-center">
+        <Navbar.Brand>
+          <img
+            className="navbar__logo"
+            src={process.env.PUBLIC_URL + "/57BlocksLogo.png"}
+            alt="57Blocks"
+          />
+        </Navbar.Brand>
+        <h2 className="navbar__title">WhatsApp Messages</h2>
+      </Navbar>
+      <Message />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Card, Form, Button, Container, Row, Col } from "react-bootstrap";
+import "./Message.css";
 
 function Message() {
   const [phone, setPhone] = useState("");
@@ -21,17 +23,34 @@ function Message() {
     window.location.href = `whatsapp://send?text=${newMessage}&phone=57${phone}}`;
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input id="phone" type="number" onChange={handleChange} value={phone} />
-        <textarea
-          id="message"
-          type="text"
-          onChange={handleChange}
-          value={message}
-        />
-        <button type="submit">Send Messagexxxxxx</button>
-      </form>
+    <div className="message__container">
+      <Card className="message__form mt-5 p-4">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="form__phone">
+            <Form.Label className="form__phone__label">Phone Number</Form.Label>
+            <Form.Control
+              className="form__phone__number mt-1 mb-4"
+              id="phone"
+              type="number"
+              onChange={handleChange}
+              value={phone}
+            />
+          </Form.Group>
+          <Form.Label className="form__message__label">Message</Form.Label>
+          <Form.Control
+            className="mt-1 mb-4"
+            as="textarea"
+            rows={5}
+            id="message"
+            type="text"
+            onChange={handleChange}
+            value={message}
+          />
+          <Button className="form__button mb-1" type="submit">
+            Send Message
+          </Button>
+        </Form>
+      </Card>
     </div>
   );
 }
